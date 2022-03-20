6e5ec9def40ae14d0b095a4209b7dd562b3b461f
pedro bzz <pedrobzz66@gmail.com>
2022-03-20 20:17:50 -0300

    feat(Responsividade): Removida as animaçoes de páginas no modo mobile, para não ficar estranho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,13 @@ import Card from "../common/components/Card";
 import Backdrop from "../common/components/Modal/Backdrop";
 import MiddleAlertModal from "../common/components/Modal/MiddleAlertModal";
 import AppContext from "../common/context/appContext";
+import useWindowSize from "../common/hooks/useWindowSize";
 import Checkout from "../modules/Checkout";
 import ShopForm from "../modules/ShopForm";
 
 const Home = (): JSX.Element => {
   const { currentPage, modal } = useContext(AppContext);
-
+  const { width } = useWindowSize();
   return (
     <>
       <AnimatePresence initial={false} exitBeforeEnter={true}>
@@ -22,8 +23,8 @@ const Home = (): JSX.Element => {
             height="780px"
             width="540px"
             decorator="wave1"
-            initialAnimation="-200vh"
-            exitAnimation="-200vh"
+            initialAnimation={width > 540 ? "-200vh" : "0"}
+            exitAnimation={width > 540 ? "-200vh" : "0"}
           >
             <ShopForm />
           </Card>
@@ -35,8 +36,8 @@ const Home = (): JSX.Element => {
             height="780px"
             width="540px"
             decorator="wave2"
-            initialAnimation="200vh"
-            exitAnimation="200vh"
+            initialAnimation={width > 540 ? "200vh" : "0"}
+            exitAnimation={width > 540 ? "200vh" : "0"}
           >
             <Checkout />
           </Card>
