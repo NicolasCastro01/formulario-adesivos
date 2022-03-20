7f2bdee4271bff0d1382abf593bbf24baa0a5f5b
pedro bzz <pedrobzz66@gmail.com>
2022-03-19 23:47:16 -0300

    feat(Aplicativo): A base do aplicativo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { AppContextProvider } from "../common/context/appContext";
+import FormLayout from "../common/layouts/FormLayout";
+
+import "../styles/styles.css";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
-      <AppContextProvider>
-        <Component {...pageProps} />
-      </AppContextProvider>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <FormLayout>
+        <AppContextProvider>
+          <Component {...pageProps} />
+        </AppContextProvider>
+      </FormLayout>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AnimatePresence } from "framer-motion";
 import { useContext } from "react";
-import { Title } from "../common/components/Title";
+import Card from "../common/components/Card";
+import Backdrop from "../common/components/Modal/Backdrop";
+import MiddleAlertModal from "../common/components/Modal/MiddleAlertModal";
 import AppContext from "../common/context/appContext";
+import ShopForm from "../modules/ShopForm";
 
 const Home = (): JSX.Element => {
-  const ctx = useContext(AppContext);
+  const { form, invalids, setForm, validateForm, setInvalids } =
+    useContext(AppContext);
   return (
     <>
-      <Title>My page</Title>
-      <h3>Context Name: {ctx.name}</h3>
+      <AnimatePresence initial={false} exitBeforeEnter={true}>
+        {invalids.length > 0 && (
+          <MiddleAlertModal
+            title="Configuração Inválida"
+            content={invalids}
+            callback={() => {
+              setInvalids([]);
+            }}
+          />
+        )}
+      </AnimatePresence>
+
+      <Card height="780px" width="540px" decorator="wave1">
+        <ShopForm />
+      </Card>
     </>
   );
 };
