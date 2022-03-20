6e5440f9d99213ef82c5184d5d745a2737c12ca6
pedro bzz <pedrobzz66@gmail.com>
2022-03-19 23:40:51 -0300

    feat(Context): Context completo para ser utilizado no formulário

--- src/common/context/appContext.tsx
+++ src/common/context/appContext.tsx
@@ -1,18 +1,57 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+export interface Form {
+  stickers: string[];
+  amount: number;
+  notes: string;
+}
 
 interface AppContextInterface {
-  name: string;
+  form: Form;
+  setForm(form: Form): void;
+  validateForm(): void;
+  invalids: string[];
+  setInvalids(invalids: string[]): void;
+  currentPage: number;
 }
 
 const AppContext = createContext<AppContextInterface>(null);
 
-const sampleAppContext: AppContextInterface = {
-  name: "TypeScript React Template",
-};
-
 export const AppContextProvider: React.FC = ({ children }) => {
+  const [invalids, setInvalids] = useState<string[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const validateForm = async () => {
+    console.log(form);
+    const newInvalids = [];
+    if (form.stickers.length === 0) {
+      newInvalids.push(
+        "Você deve escolher, pelo menos, um adesivo para continuar com o pedido.",
+      );
+    }
+    if (form.amount === 0) {
+      newInvalids.push(
+        "Para finalizar o pedido é necessario especificar uma quantidade",
+      );
+    }
+    setInvalids(newInvalids);
+    if (!newInvalids.length) setCurrentPage(1);
+  };
+  const [form, setForm] = useState<Form>({
+    stickers: [],
+    amount: 1,
+    notes: "",
+  });
   return (
-    <AppContext.Provider value={sampleAppContext}>
+    <AppContext.Provider
+      value={{
+        form,
+        setForm,
+        validateForm,
+        invalids,
+        setInvalids,
+        currentPage,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
